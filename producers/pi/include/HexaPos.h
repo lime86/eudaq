22913530a7375de5b84924dc1d52e1b59e089a5c
Artur Lobanov <arturlbv@gmail.com>
2018-03-06 21:25:02 +0100

    Class to produce hexagonal grid

---
+++ producers/pi/include/HexaPos.h
@@ -0,0 +1,79 @@
+#ifndef HEXAPOS_H
+#define HEXAPOS_H
+
+// system includes
+#include <vector>
+
+// Hexagonal position list
+class HexaPos {
+    /*
+     _______
+    /   |   \
+   /    |    \
+  /_____|_____\
+  \     |     /
+   \    |    /
+    \   |   /
+     -------
+
+    */
+
+protected:
+
+    // Hexagon parameters
+    const double m_largeD = 1350.;
+    const double m_smallD = 1250.;
+
+    const double m_largeR = m_largeD / 2.;
+    const double m_smallR = m_smallD / 2.;
+
+    // side parameters
+    // y = ax + b for the top-right edge
+    // x = (y-b)/a
+    const double m_b =  2. * m_smallR;
+    const double m_a = -2. * m_smallR/m_largeR;
+
+    // center positions
+    double m_centerX = 0.;
+    double m_centerY = 0.;
+
+    // step sizes
+    double m_stepSizeX = 50.;
+    double m_stepSizeY = 25.;
+
+    // border offset
+    double m_borderOffsetX = 0;
+    double m_borderOffsetY = 0;
+
+    // start positions
+    double m_startY = m_smallR - m_borderOffsetY;
+    double m_startX = - (m_startY - m_b) / m_a + m_borderOffsetX;
+
+    // Position list/vector
+    std::vector<double> m_positionsX;
+    std::vector<double> m_positionsY;
+
+    std::vector< std::vector<double> > m_positions;
+
+    unsigned int m_npoints = 0;
+
+public:
+
+    HexaPos();
+    HexaPos(double centerX, double centerY);
+
+    ~HexaPos();
+
+    int BuildGrid();
+
+    // functions to set parameters
+    void setStepX(int step) { m_stepSizeX = step; };
+    void setStepY(int step) { m_stepSizeY = step; };
+
+    // functions to get parameters
+    int getPosX(int positionID) { return (positionID < m_npoints)? m_positionsX.at(positionID):-1; };
+    int getPosY(int positionID) { return (positionID < m_npoints)? m_positionsY.at(positionID):-1; };
+    int getNpos() {return m_npoints;};
+};
+
+#endif
---
+++ producers/pi/src/HexaPos.cc
@@ -0,0 +1,86 @@
+#include "HexaPos.h"
+#include <iostream>
+
+/* Constructor and destructor */
+
+HexaPos::HexaPos() {}
+
+HexaPos::HexaPos(double centerX, double centerY) {
+
+    m_centerX = centerX;
+    m_centerY = centerY;
+}
+
+HexaPos::~HexaPos() {}
+
+/* Construct position grid */
+int HexaPos::BuildGrid() {
+
+    /*
+     _______
+    /   |   \
+   /    |    \
+  /_____|_____\
+  \     |     /
+   \    |    /
+    \   |   /
+     -------
+
+    */
+
+    double posX = m_startX;
+    double posY = m_startY;
+
+    while(posY > - (m_smallR + m_borderOffsetY)){
+
+	if (posY < m_smallR &&
+	    posY > -m_smallR &&
+	    posX > -m_largeD &&
+	    posX < m_largeD){
+
+	    m_npoints += 1;
+
+	    m_positionsX.push_back(posX + m_centerX);
+	    m_positionsY.push_back(posY + m_centerY);
+	    //std::vector<double> posXY{ posX, posY};
+	    //m_positions.push_back(posXY);
+	}
+
+
+	// Move
+	if (posY > 0){
+	    // Reached end of row, moving to the start of the next one
+	    if ( posY > m_a * posX + m_b){
+		posY -= m_stepSizeY;
+		posX = - (posY - m_b) / m_a + m_borderOffsetX;
+	    }
+	    else {
+		posX += m_stepSizeX;
+	    }
+	}
+	else {
+	    // Reached end of row, moving to the start of the next one
+	    if ( posY < -(m_a * posX + m_b)){
+		posY -= m_stepSizeY;
+		posX = (posY + m_b) / m_a + m_borderOffsetX;
+	    }
+	    else {
+		posX += m_stepSizeX;
+	    }
+	}
+
+	//if (m_npoints > 100) break;
+    }
+
+    return 0;
+}
+
+/*
+int getPosX(int positionID){
+
+    if(positionID < m_npoints)
+	return m_positionsX[positionID];
+    else
+	return -1;
+}
+*/
